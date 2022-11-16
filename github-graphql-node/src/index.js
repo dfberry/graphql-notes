28b0788ec48c1236f7b91000a3f8dc5138ed3fe9
4ttedertlf <107864453+4ttedertlf@users.noreply.github.com>
2022-11-15 20:36:25 -0800

    loop thru all records in query

--- github-graphql-node/src/index.js
+++ github-graphql-node/src/index.js
@@ -4,21 +4,17 @@ require('dotenv').config();
 const { QUERY_ORG_REPOS_AGGREGATE_VARIABLES,QUERY_ORG_REPOS_AGGREGATE } = require ("./queries");
 
 const personal_access_token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
-if(!personal_access_token) throw Error("missing personal access token");
-
+if(!personal_access_token) throw Error("missing personal access token GITHUB_PERSONAL_ACCESS_TOKEN");
 
 const GRAPHQL_URL = process.env.GITHUB_GRAPHQL_ENDPOINT;
+if(!GRAPHQL_URL) throw Error("missing GRAPHQL_URL");
+
 const QUERY = QUERY_ORG_REPOS_AGGREGATE;
 const VARIABLES = QUERY_ORG_REPOS_AGGREGATE_VARIABLES;
 
 
 // Assume variable for next cursor is `nextCursor`
-async function getGraphQLCursor(url, personal_access_token, query, variables, nextCursor){
-
-  // Set cursor
-  if(nextCursor && ((typeof nextCursor)==='string') && (nextCursor.length>0)){
-    variables.nextCursor = nextCursor;
-  }
+async function getGraphQLCursor(url, personal_access_token, query, variables){
 
   // Prepare graphQL query for axios
   var data = JSON.stringify({
@@ -43,9 +39,41 @@ async function getGraphQLCursor(url, personal_access_token, query, variables, ne
 
 }
 
-getGraphQLCursor(GRAPHQL_URL, personal_access_token, QUERY, VARIABLES, "")
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
+async function loopThruCursors(){
+  let hasNextPage = true; 
+  let data = [];
+  VARIABLES.after = null;
+
+  do{
+
+    const response = await getGraphQLCursor(GRAPHQL_URL, personal_access_token, QUERY, VARIABLES);
+
+    if(!response.data.error && response.status == 200 && 
+      (response.data.data && response.data.data.organization && response.data.data.organization.repositories)){
+
+        console.log(`returned: ${response.data.data.organization.repositories.pageInfo.startCursor}-${response.data.data.organization.repositories.pageInfo.hasNextPage}-${response.data.data.organization.repositories.pageInfo.endCursor}-${response.data.data.organization.repositories.edges.length}`)
+        console.log(`${response.data.data.organization.repositories.edges[0].node.repositoryName}`)
+        const graphQLData = response.data.data;
+  
+        hasNextPage = graphQLData.organization.repositories.pageInfo.hasNextPage;
+        VARIABLES.after = graphQLData.organization.repositories.pageInfo.endCursor;
+    
+        const flattenEdge = graphQLData.organization.repositories.edges.map(edge => edge.node);
+        data.push(...flattenEdge);
+
+    } else {
+      throw Error(response.data.error);
+    }
+    
+
+  } while (hasNextPage)
+
+  return data;
+}
+
+loopThruCursors()
+.then(function (repositories) {
+  console.log(repositories);
 })
 .catch(function (error) {
   console.log(error);
--- github-graphql-node/src/queries.js
+++ github-graphql-node/src/queries.js
@@ -1,13 +1,13 @@
 const QUERY_ORG_REPOS_AGGREGATE_VARIABLES= {
   "organization": "Azure-Samples",
   "after": "",
-  "pageSize": 100
+  "pageSize": parseInt(process.env.GRAPHQL_CURSOR_SIZE) || 100 
 }
 
 const QUERY_ORG_REPOS_AGGREGATE = `
-query OrgReposMegaInfoExample($organization:String!, $pageSize: Int) {
-    organization(login:$organization){
-      repositories (first: $pageSize, orderBy: {field: STARGAZERS, direction: DESC}){
+query OrgReposMegaInfoExample($organization:String!, $pageSize: Int, $after: String) {
+  organization(login:$organization){
+    repositories (after:$after, first: $pageSize, orderBy: {field: STARGAZERS, direction: DESC}){
         totalCount
         pageInfo {
           startCursor
